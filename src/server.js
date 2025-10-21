40bfa36280df4fa1217f1fea9c5384504050845b
felype_cybher <felypemaxsantossilva0@gmail.com>
2025-10-21 17:45:15 -0300

    fix: Binds server to 0.0.0.0 for Render compatibility

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const app = require('./app');
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
+app.listen(PORT,'0.0.0.0', () => {
+  console.log(`Servidor rodando na porta,  ${PORT}`);
   console.log('Backend do Guia Santa Cruz está online! 🚀');
 });
